4108ade0dc027f3901ed35f5705b3d3cdce3ade1
Victor Canas <victorcanas972@gmail.com>
2022-07-05 20:39:33 +0200

    Add _document page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="es">
+        <Head>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+} 
