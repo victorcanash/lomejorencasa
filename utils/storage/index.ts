97dd0a81da395ea4c25e84526b0f8187f88c33aa
Victor Canas <victorcanas972@gmail.com>
2022-07-12 19:54:50 +0200

    storage functions in utils

---
+++ utils/storage/index.ts
@@ -0,0 +1,16 @@
+export const getLocalStorageItem = (key: string): any | boolean => {
+  const storedValue = localStorage.getItem(key);
+  if(!storedValue){
+      return false;
+  }else{
+      return storedValue;
+  }
+};
+
+export const setLocalStorageItem = (key: string, value: string): any | boolean => {
+  localStorage.setItem(key, value);
+};
+
+export const removeLocalStorageItem = (key: string): any | boolean => {
+  localStorage.removeItem(key);
+}
