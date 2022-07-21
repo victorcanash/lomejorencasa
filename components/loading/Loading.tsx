f9f76dea2e2ff79a614d0f5f60248a5c941970af
Victor Canas <victorcanas972@gmail.com>
2022-07-21 20:25:49 +0200

    loading component

---
+++ components/loading/Loading.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import CircularProgress from '@mui/material/CircularProgress';
+
+
+export const Loading = () => {
+
+  return (
+    <div 
+      style={{
+        position: 'fixed',
+        top: '0px',
+        left: '0px',
+        zIndex: '10000',
+        width: '100vw', 
+        height: '100vh',
+        backgroundColor: 'rgba(255, 255, 255, 0.7)', 
+        display:'flex', 
+        placeContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      <CircularProgress 
+        size='60px' 
+        sx={{
+          color: 'primary.main'
+        }}
+        
+      />
+    </div>
+  );
+};
