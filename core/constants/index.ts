65c439fb4fb904378c56e52d25fb0b5f2d948861
Victor Canas <victorcanas972@gmail.com>
2022-07-21 20:27:43 +0200

    constants and types in core

---
+++ core/constants/index.ts
@@ -0,0 +1,53 @@
+import * as Yup from 'yup';
+
+export const loginValidation = Yup.object().shape(
+  {
+    email: Yup
+      .string()
+      .email('Invalid email format')
+      .required('Email is required'),
+    password: Yup
+      .string()
+      .min(8, 'Password too short')
+      .required('Password is required'),
+  }
+);
+
+export const registerValidation = Yup.object().shape(
+  {
+    firstName: Yup
+      .string()
+      .min(3, 'First name must have 3 letters minimum')
+      .max(12, 'First name must have maximum 12 letters')
+      .required('First name is required'),
+    lastName: Yup
+      .string()
+      .min(3, 'Last name must have 3 letters minimum')
+      .max(12, 'Last name must have maximum 12 letters')
+      .required('Last name is required'),
+    email: Yup
+      .string()
+      .email('Invalid email format')
+      .required('Email is required'),
+    password: Yup
+      .string()
+      .min(8, 'Password too short')
+      .required('Password is required'),
+    confirm: Yup
+      .string()
+      .when("password", {
+          is: (value: string) => (value && value.length > 0 ? true : false),
+          then: Yup.string().oneOf(
+              [Yup.ref("password")], 'Passwords must match'
+          )
+      })
+      .required('You mus confirm your password'),
+    age: Yup.number()
+      .min(10, 'You must be over 10 years old')
+      .required('Age is required')
+  }
+);
+
+export const JWTTokenKey = 'sessionJWTToken'
+
+export const maxCartQuantity = 99
---
+++ core/types/index.ts
@@ -0,0 +1,107 @@
+export type User = {
+  id: number;
+  email: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+  addresses: UserAddress[];
+  payments: UserPayment[];
+  cart: Cart;
+}
+
+export type UserUpdate = {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
+
+export type AuthLogin = {
+  email: string;
+  password: string;
+};
+
+export type AuthRegister = {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+}
+
+
+export type UserAddress = {
+  id: number;
+  userId: number;
+  addressLine: string;
+  additionalInfo: string;
+  postalCode: string;
+  locality: string;
+  administrativeArea: string;
+  country: string;
+  type?: string;
+}
+
+
+export type UserPayment = {
+  id: number;
+  userId: number;
+  type: string;
+  provider: string;
+  accountNumber: string;
+  expiry: Date
+}
+
+
+export type Cart = {
+  id: number;
+  userId: number;
+  total: number;
+  items: CartItem[];
+}
+
+
+export type CartItem = {
+  id: number;
+  cartId: number;
+  productId: number;
+  product: Product;
+  quantity: number;
+}
+
+
+export type Product = {
+  id: number;
+  categoryId: number;
+  name: string;
+  description: string;
+  sku: string;
+  price: number;
+  imageNames: string[];
+  inventories: ProductInventory[];
+  discount?: ProductDiscount;
+};
+
+
+export type ProductCategory = {
+  id: number;
+  name: string;
+  description: string;
+}
+
+
+export type ProductInventory = {
+  id: number;
+  productId: number;
+  quantity: number;
+  size?: string;
+}
+
+
+export type ProductDiscount = {
+  id: number;
+  productId: number;
+  name: string;
+  description: string;
+  discountPercent: number;
+  active: boolean;
+}
