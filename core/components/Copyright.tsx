a2d14c12bab08f85f18da37bc9ddd424a31fc22f
Victor Canas <victorcanas972@gmail.com>
2022-07-22 12:56:50 +0200

    Core components as arrow functions

--- core/components/Copyright.tsx
+++ core/components/Copyright.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
+import React from 'react';
 import Typography from '@mui/material/Typography';
 import MuiLink from '@mui/material/Link';
 
-export default function Copyright() {
+const Copyright = () => {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
@@ -13,3 +13,5 @@ export default function Copyright() {
     </Typography>
   );
 }
+
+export default Copyright;
