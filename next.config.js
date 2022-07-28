2441221c6535c28c62297542b6dbf90938eccc8e
Victor Canas <victorcanas972@gmail.com>
2022-07-28 19:45:32 +0200

    add images domain from back

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['ecommerce-vc-api.herokuapp.com'],
+  },
 }
 
 module.exports = nextConfig
