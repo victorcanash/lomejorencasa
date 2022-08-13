8540e552174541658f2f14c8f81d710a0ee076c2
Victor Canas <victorcanas972@gmail.com>
2022-08-13 11:49:34 +0200

    Set app props in usePage

--- lib/hooks/usePage.tsx
+++ lib/hooks/usePage.tsx
@@ -1,16 +1,22 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
+import { PageProps } from '@lib/server/page';
 import { useAppContext } from '@lib/contexts/AppContext';
 
-const usePage = () => {
-  const { setLoading } = useAppContext();
+const usePage = (pageProps?: PageProps) => {
+  const { setLoading, setToken, setUser, setCategories } = useAppContext();
 
   const router = useRouter();
 
   useEffect(() => {
+    if (pageProps && pageProps.categories && pageProps.categories.length > 0) {
+      setToken(pageProps.token);
+      setUser(pageProps.user || undefined);
+      setCategories(pageProps.categories);
+    }
     setLoading(false);  
-  }, [router.asPath, setLoading]);
+  }, [pageProps, pageProps?.categories, pageProps?.token, pageProps?.user, router.asPath, setCategories, setLoading, setToken, setUser]);
 
   return {};
 };
