4c5f5a81beebf803be2223241eeea9db8a603bb4
Victor Canas <victorcanas972@gmail.com>
2022-08-13 18:03:57 +0200

    Cart middlewares

---
+++ core/middlewares/cart/index.ts
@@ -0,0 +1,40 @@
+import { AxiosRequestConfig } from 'axios'
+
+import axios from '@core/config/axios.config'
+import type { Cart, CartItem } from '@core/types/cart'
+
+export const createCart = (token: string) => {
+  const options: AxiosRequestConfig = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+  return axios.post('/carts', undefined, options)
+}
+
+export const createCartItem = (token: string, cartItem: CartItem) => {
+  const options: AxiosRequestConfig = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+  return axios.post('/citems', cartItem, options)
+}
+
+export const updateCartItem = (token: string, cartItem: CartItem) => {
+  const options: AxiosRequestConfig = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+  return axios.put(`/citems/${cartItem.id}`, cartItem, options)
+}
+
+export const deleteCartItem = (token: string, id: number) => {
+  const options: AxiosRequestConfig = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+  return axios.delete(`/citems/${id}`, options)
+}
