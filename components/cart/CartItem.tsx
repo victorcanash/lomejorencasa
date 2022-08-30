ad01bec231c75c00362b38b72b7e9b1923b7dcd5
Victor Canas <victorcanas972@gmail.com>
2022-08-30 17:44:30 +0200

    Add cart page

---
+++ components/cart/CartItem.tsx
@@ -0,0 +1,113 @@
+import Image from 'next/image';
+
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+import { CartItem } from '@core/types/cart';
+import { getProductImgUrl, getProductPrice } from '@core/utils/products';
+import Link from '@core/components/Link';
+
+type CartItemProps = {
+  item: CartItem,
+  updateQuantity: (cartItem: CartItem, quantity: number) => void,
+};
+
+const CartItem = (props: CartItemProps) => {
+  const { item, updateQuantity } = props;
+
+  const handleRemoveItem = () => {
+    updateQuantity(item, 0);
+  };
+
+  const handleSelectQuantity = (event: SelectChangeEvent) => {
+    const quantity = parseInt(event.target.value);
+    updateQuantity(item, quantity);
+  };
+
+  const getMenuItems = () => {
+    const menuItems = [];
+    for (let i = 0; i < item.inventory.quantity; i++) {
+      menuItems.push(
+        <MenuItem key={i+1} value={i+1}>
+          {i+1}
+        </MenuItem>
+      );
+    }
+    return menuItems;
+  }
+
+  return (
+    <>
+      <Grid container spacing={2}>
+
+        <Grid
+          item
+        >
+          <Link href={`/products/${item.product.name}?id=${item.product.id}`} noLinkStyle>
+          <div style={{ position: 'relative', minWidth: '100px' }}>
+            <Image
+              src={getProductImgUrl(item.product)}
+              alt="Product image"
+              width="500"
+              height="500"
+              layout="responsive"
+              objectFit="cover"
+            />
+          </div>
+          </Link>
+        </Grid>
+
+        <Grid item xs={12} sm container>
+
+          <Grid item xs container direction="column" spacing={2}>
+
+            <Grid item xs>
+              <Typography gutterBottom variant="subtitle1" component="div">
+                {item.product.name}
+              </Typography>
+              <Typography variant="body2">
+                Size: {item.inventory.size || 'Unique'}
+              </Typography>
+            </Grid>
+
+            <Grid item>
+              <Select
+                labelId="quantity-select-label"
+                id="quantity-select"
+                value={item.quantity.toString()}
+                label="Quantity"
+                onChange={handleSelectQuantity}
+              >
+                {getMenuItems()}
+              </Select>
+
+              <Tooltip title='Delete' placement='top'>
+                <IconButton 
+                  onClick={handleRemoveItem}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </Tooltip>
+            </Grid>
+
+          </Grid>
+
+          <Grid item>
+            <Typography variant="subtitle1" component="div">
+              {(getProductPrice(item.product) * item.quantity).toFixed(2)} €
+            </Typography>
+          </Grid>
+
+        </Grid>
+
+      </Grid>
+    </>
+  );
+};
+
+export default CartItem;
---
+++ pages/cart.tsx
@@ -0,0 +1,84 @@
+import { Fragment, useState } from 'react';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import PointOfSaleIcon from '@mui/icons-material/PointOfSale';
+
+import LinkButton from '@core/components/LinkButton';
+import { useCartContext } from '@lib/contexts/CartContext';
+import usePage from '@lib/hooks/usePage';
+import useCart from '@lib/hooks/useCart';
+import CartItem from '@components/cart/CartItem';
+import GoBackBtn from '@components/ui/GoBackBtn';
+
+const Cart: NextPage = () => {
+  const { cart, totalPrice, totalQuantity } = useCartContext();
+
+  const page = usePage();
+  const { updateCartItemQuantity } = useCart();
+
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <>
+      <Head>
+        <title>Cart</title>
+        <meta name="description" content="Cart page" />
+      </Head>
+
+      <Typography component='h1' variant='h4'>
+        {`My cart (${totalQuantity})`}
+      </Typography>
+      <Divider sx={{ my: 3 }} />
+
+      {cart && cart.items.length > 0 ? (
+        <>
+          <Container className='animate__animated animate__fadeIn'>
+            {cart.items.map((item) => (
+              <Fragment key={item.id}>
+                <CartItem 
+                  item={item} 
+                  updateQuantity={updateCartItemQuantity}
+                />
+                <Divider variant='fullWidth' sx={{ my: 3 }} />
+              </Fragment>
+            ))}
+          </Container>
+
+          <Typography
+            variant='h6'
+            align='right'
+            className='animate__animated animate__fadeInUp'
+          >
+            Total: {totalPrice.toFixed(2)} €
+          </Typography>
+
+          <Box display='flex' justifyContent={'center'} my={1}>
+            <LinkButton
+              href='/checkout'
+              startIcon={<PointOfSaleIcon />}
+            >
+              Proceed to payment
+            </LinkButton>
+          </Box>
+
+          <GoBackBtn />
+        </>
+      ) : (
+        <>
+          <Typography variant='h5' align='center' sx={{ my: 5 }}>
+            There are no products added
+          </Typography>
+          <GoBackBtn />
+        </>
+      )}
+    </>
+  );
+};
+
+export default Cart;
