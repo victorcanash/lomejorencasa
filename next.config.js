05f99bbfeec5549933f38fbc22dceac2090708ea
Victor Canas <victorcanas972@gmail.com>
2022-09-08 14:10:38 +0200

    add localhost domain in images

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['ecommerce-vc-api.herokuapp.com'],
+    domains: [
+      'localhost', 
+      'ecommerce-vc-api.herokuapp.com'
+    ],
   },
 }
 
