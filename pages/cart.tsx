eff9a6776b861ef91cf955da9e799396fe1a411a
Victor Canas <victorcanas972@gmail.com>
2022-09-21 20:09:39 +0200

    Cart content in a separate component

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,25 +1,11 @@
-import { Fragment } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Divider from '@mui/material/Divider';
-import Typography from '@mui/material/Typography';
-
-import { useCartContext } from '@lib/contexts/CartContext';
 import usePage from '@lib/hooks/usePage';
-import useCart from '@lib/hooks/useCart';
-import CartItem from '@components/cart/CartItem';
-import GoBackBtn from '@components/ui/GoBackBtn';
-import CheckoutBtn from '@components/checkout/CheckoutBtn';
-import envConfig from '@core/config/env.config';
+import CartDetail from '@components/cart/CartDetail';
 
 const Cart: NextPage = () => {
-  const { cart, totalPrice, totalQuantity } = useCartContext();
-
   const page = usePage();
-  const { updateCartItemQuantity } = useCart();
 
   return (
     <>
@@ -28,47 +14,7 @@ const Cart: NextPage = () => {
         <meta name="description" content="Cart page" />
       </Head>
 
-      <Typography component='h1' variant='h5' className='animate__animated animate__fadeInLeft'>
-        {`My cart (${totalQuantity})`}
-      </Typography>
-      <Divider sx={{ my: 3 }} />
-
-      {cart && cart.items.length > 0 ? (
-        <>
-          <Container className='animate__animated animate__fadeIn'>
-            {cart.items.map((item) => (
-              <Fragment key={item.id}>
-                <CartItem 
-                  item={item} 
-                  updateQuantity={updateCartItemQuantity}
-                />
-                <Divider variant='fullWidth' sx={{ my: 3 }} />
-              </Fragment>
-            ))}
-          </Container>
-
-          <Typography
-            variant='h6'
-            align='right'
-            className='animate__animated animate__fadeInUp'
-          >
-            Total: {totalPrice.toFixed(2)} €
-          </Typography>
-
-          <Box display='flex' justifyContent={'center'} my={1}>
-            <CheckoutBtn />
-          </Box>
-
-          <GoBackBtn />
-        </>
-      ) : (
-        <>
-          <Typography variant='h5' align='center' sx={{ my: 5 }}>
-            There are no products added
-          </Typography>
-          <GoBackBtn />
-        </>
-      )}
+      <CartDetail />
     </>
   );
 };
