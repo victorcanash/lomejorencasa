e4de666435538d691e9eb985106eb5d1f28d35f8
Victor Canas <victorcanas972@gmail.com>
2022-09-23 16:37:02 +0200

    newemail page without layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -48,7 +48,11 @@ function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   const { asPath } = useRouter();
 
-  const isLayoutNeeded = ![`${RouterPaths.activation}`, `${RouterPaths.reset}`].includes(props.router.pathname);
+  const isLayoutNeeded = ![
+    `${RouterPaths.activation}`, 
+    `${RouterPaths.reset}`, 
+    `${RouterPaths.newemail}`]
+    .includes(props.router.pathname);
   const LayoutComponent = isLayoutNeeded ? Layout : Fragment;
 
   return (
