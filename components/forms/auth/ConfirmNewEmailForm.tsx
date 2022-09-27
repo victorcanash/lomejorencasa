eb57fec5a317b6eafc2da885ecd09629b070b9b0
Victor Canas <victorcanas972@gmail.com>
2022-09-27 17:31:47 +0200

    new email form in a separated component

---
+++ components/forms/auth/ConfirmNewEmailForm.tsx
@@ -0,0 +1,65 @@
+import { useRouter } from 'next/router';
+
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
+
+import useAuth from '@lib/hooks/useAuth';
+
+const ConfirmNewEmailForm = () => {
+  const router = useRouter();
+  const { updateEmail, errorMsg, successMsg } = useAuth();
+
+  const handleSubmit = async () => {
+    const token = typeof router.query.token == 'string' ? router.query.token : '';
+    updateEmail(token);
+  }
+
+  return (
+    <Container maxWidth="lg">
+      
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+      
+        <Typography component="h1" variant="h5">
+          Email update link
+        </Typography>
+
+        <Typography component="h2" variant="subtitle1" mt={4}>
+          Click the confirm button to update and activate your new email
+        </Typography>
+
+        <Button
+          variant="contained"
+          onClick={handleSubmit}
+          sx={{
+            mt: 4,
+            mb: 2,
+          }}
+        >
+          Confirm
+        </Button>
+
+        {
+          errorMsg && errorMsg !== '' &&
+            <Alert severity="error">{ errorMsg }</Alert>
+        }  
+        {
+          successMsg && successMsg !== '' &&
+            <Alert>{ successMsg }</Alert>
+        } 
+
+      </Box>
+
+    </Container>
+  );
+};
+
+export default ConfirmNewEmailForm;
--- pages/newemail.tsx
+++ pages/newemail.tsx
@@ -1,81 +1,26 @@
-import { useRouter } from 'next/router';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import Alert from '@mui/material/Alert';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import ErrorIcon from '@mui/icons-material/Error';
 
 import usePage from '@lib/hooks/usePage';
-import useAuth from '@lib/hooks/useAuth';
+import ConfirmNewEmailForm from '@components/forms/auth/ConfirmNewEmailForm';
 
 const NewEmail: NextPage = () => {
-  const router = useRouter();
   const page = usePage();
-  const { updateEmail, errorMsg, successMsg } = useAuth();
-
-  const onClickUpdateBtn = () => {
-    const token = typeof router.query.token == 'string' ? router.query.token : '';
-    updateEmail(token);
-  }
 
   return (
     <>
       <Head>
-        <title>Email update</title>
-        <meta name="description" content="Email update page" />
+        <title>Confirm new email</title>
+        <meta name="description" content="Confirm new email page" />
       </Head>
 
       <Container 
         maxWidth={false}
-        className="centeredPage"
+        className="noLayoutPage"
       >
-        <Avatar 
-          sx={{ 
-            mb: 1, 
-            bgcolor: 'secondary.main' 
-          }}
-        >
-          {
-            successMsg != '' ?
-              <CheckCircleIcon />
-              :
-              <ErrorIcon />
-          }
-        </Avatar>
-
-        <Typography component="h1" variant="h5">
-          Email update link
-        </Typography>
-
-        <Typography component="h2" variant="subtitle1" mt={4}>
-          Click the confirm button to update and activate your new email
-        </Typography>
-
-        <Button
-          variant="contained"
-          onClick={onClickUpdateBtn}
-          sx={{
-            mt: 4,
-            mb: 2,
-          }}
-        >
-          Confirm
-        </Button>
-
-        {
-          errorMsg && errorMsg !== '' &&
-            <Alert severity="error">{ errorMsg }</Alert>
-        }  
-        {
-          successMsg && successMsg !== '' &&
-            <Alert>{ successMsg }</Alert>
-        } 
-
+        <ConfirmNewEmailForm />
       </Container>
     </>
   );
