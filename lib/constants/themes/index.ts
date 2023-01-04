dcfdf42bd5c8c5879176f9eee494d4a00fd70e3f
VictorCanas <victorcanas972@gmail.com>
2023-01-04 18:01:51 +0100

    styling general

--- lib/constants/themes/index.ts
+++ lib/constants/themes/index.ts
@@ -2,31 +2,158 @@ import { createTheme, ThemeOptions } from '@mui/material/styles';
 
 export const themeOptions: ThemeOptions = {
   palette: {
-    mode: 'dark',
     primary: {
-      main: '#91002e',
+      main: '#F1F4ED',
     },
     secondary: {
-      main: '#3d0043',
+      main: '#ecf7dc',
     },
     background: {
-      default: '#282c34',
-      //paper: '#2d2d2d',
-      paper: '#91002e',
+      default: '#F1F4ED',
+      paper: '#ecf7dc',
     },
     text: {
-      primary: '#ffffff',
-      secondary: '#ffffff',
-      disabled: '#bdbdbd',
+      primary: '#000000',
+      secondary: '#000000',
+      disabled: '#000000',
     },
-    divider: '#ffffff',
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          fontSize: '15px',
+          lineHeight: '18px',
+          fontWeight: '700',
+          textTransform: 'none',
+          backgroundColor: '#FFFFFF',
+          '&:hover': {
+            backgroundColor: '#ecf7dc',
+          },
+        },
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          fontSize: '15px',
+          lineHeight: '1.5em',
+          fontWeight: '300',
+          '&.Mui-focused': {
+            color: '#000000',
+          },
+        },
+      },
+    },
+    MuiInputBase: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#FFFFFF',
+          '&:hover': {
+            backgroundColor: '#ecf7dc',
+          },
+          '&.Mui-focused': {
+            backgroundColor: '#ecf7dc',
+          },
+        },
+      },
+    },
+    MuiAvatar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#d7e4c5',
+        },
+      },
+    },
+    MuiSvgIcon: {
+      styleOverrides: {
+        root: {
+          fill: '#000000',
+        },
+      },
+    },
+    MuiStepIcon: {
+      styleOverrides: {
+        root: {
+          fill: '#d1e7b1',
+          fontSize: '25px',
+        },
+      },
+    },
+    MuiStepLabel: {
+      styleOverrides: {
+        label: {
+          fontSize: '20px',
+        },
+      },
+    },
+    MuiDivider: {
+      styleOverrides: {
+        root: {
+          border: '1px solid #b0b0b0',
+        },
+      },
+    },
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          textDecoration: 'underline',
+          color: '#000000',
+        },
+      },
+    },
+    MuiAlert: {
+      styleOverrides: {
+        root: {
+          fontSize: '15px',
+          lineHeight: '20px',     
+        },
+        standardSuccess: {
+          backgroundColor: 'rgb(209 255 209)',
+        },
+        standardError: {
+          backgroundColor: 'rgb(255 199 199)',
+        },
+      },
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          letterSpacing: '-0.33px',
+        },
+      },
+    },
   },
   typography: {
+    // Page title
+    h1: {
+      fontSize: '25px',
+      lineHeight: '28px',
+      fontWeight: '500',
+    },
+    // Navbar title
+    subtitle1: {
+      fontSize: '30px',
+      lineHeight: '33px',
+      fontWeight: '800',
+    },
+    // Page content
+    body1: {
+      fontSize: '15px',
+      lineHeight: '18px',
+      fontWeight: '300',
+    },
+    // Page content 2
+    body2: {
+      fontSize: '13px',
+      lineHeight: '16px',
+      fontWeight: '300',
+    },
     fontFamily: [
+      'Roboto',
       '-apple-system', 
       'BlinkMacSystemFont', 
       'Segoe UI', 
-      'Roboto', 
       'Oxygen',
       'Ubuntu', 
       'Cantarell', 
