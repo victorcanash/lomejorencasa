002644051110a9df5068011cdab87c39a4ba7241
Victor Canas <victorcanas@Air-de-Victor.home>
2023-01-11 19:04:14 +0100

    Product detail page style

--- components/products/ProductDetail.tsx
+++ components/products/ProductDetail.tsx
@@ -5,13 +5,14 @@ import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import Box from '@mui/material/Box';
 
 import { Product } from '@core/types/products';
 import { getProductImgUrl } from '@core/utils/products';
 import useCart from '@lib/hooks/useCart';
 import useSelectInventory from '@lib/hooks/useSelectInventory';
-import GoBackBtn from '@components/ui/GoBackBtn';
-import ProductDescription from '@components/products/ProductDescription';
 import Carousel from '@components/ui/Carousel';
 
 type ProductDetailProps = {
@@ -29,6 +30,11 @@ const ProductDetail = (props: ProductDetailProps) => {
       addCartItem(selectedInventory);
     }
   };
+  const divider = <Divider sx={{ mt: 4, mb: 4 }}/>
+
+  const liStyle = {
+    marginBottom: '10px',
+  };
 
   return (
     <>
@@ -38,12 +44,14 @@ const ProductDetail = (props: ProductDetailProps) => {
         className='animate__animated animate__fadeIn'
         spacing={3}
       >
+
         <Grid
           item
           xs={12}
           sm={12}
           md={6}
           className='animate__animated animate__fadeInLeft'
+          sx= {{ mb: 1 }}
         >
           <Card raised>
             <CardMedia>
@@ -52,9 +60,8 @@ const ProductDetail = (props: ProductDetailProps) => {
               />
             </CardMedia>
           </Card>
-          <br/>
-          <GoBackBtn />
         </Grid>
+
         <Grid 
           item 
           xs={12} 
@@ -62,10 +69,34 @@ const ProductDetail = (props: ProductDetailProps) => {
           md={6}
           className='animate__animated animate__fadeInRight'
         >
-          <ProductDescription product={product} detailed={true} selectedInventory={selectedInventory} />
+          <Typography component={"h1"} variant={"subtitle1"} sx={{ mb: 2 }}>
+            <FormattedMessage id="productDetail.h1" />
+          </Typography>    
+          { product.activeDiscount ?
+            <Box sx={{ mb: 2 }}>
+              <Typography component={"h2"} variant={"h1"} color="error">
+                {selectedInventory ? selectedInventory.realPrice : product.lowestRealPrice} €
+              </Typography>
+              <Typography component={"span"} variant={"body1"}>
+                <FormattedMessage id="productDetail.original" />: <s>{selectedInventory ? selectedInventory.price : product.lowestPrice} €</s>
+              </Typography> 
+              <Typography component={"span"} variant={"body1"} color="error"> 
+                {` -${product.activeDiscount.discountPercent}%`}
+              </Typography> 
+            </Box>
+            :
+            <Box sx={{ mb: 2 }}>
+              <Typography component={"h2"} variant={"h1"}>
+                {selectedInventory ? selectedInventory.realPrice : product.lowestRealPrice} €
+              </Typography>
+            </Box>
+          }
+          <Typography component="h2" variant="body1" sx={{ mb: 3 }}>
+            <FormattedMessage id="productDetail.description" />
+          </Typography>
           { loaded &&
             <FormControl 
-              sx={{ mt: 3, mb: 2 }} 
+              sx={{ mb: 3 }} 
               className='animate__animated animate__fadeIn'
             >
               <Select />
@@ -80,8 +111,164 @@ const ProductDetail = (props: ProductDetailProps) => {
               </Button>
             </FormControl>
           }
+          <Typography component="div" variant="body1" sx={{ mb: 1 }}>
+            <FormattedMessage id="productDetail.comment" />
+          </Typography>
         </Grid>
+      
       </Grid>
+
+        <Grid 
+          item 
+          xs={12} 
+          sm={12} 
+          md={6}
+          className='animate__animated animate__fadeInRight'
+        >
+          {divider}
+          <Typography component="div" variant="h1">
+            <FormattedMessage id="productDetail.details.title" />
+          </Typography>
+          <ul>
+            <Typography component="li" variant="body1" style={liStyle}>
+              <FormattedMessage id="productDetail.details.1" />
+            </Typography>
+            <Typography component="li" variant="body1" style={liStyle}>
+              <FormattedMessage id="productDetail.details.2" />
+            </Typography>
+            <Typography component="li" variant="body1" style={liStyle}>
+              <FormattedMessage id="productDetail.details.3" />
+            </Typography>
+            <Typography component="li" variant="body1" style={liStyle}>
+              <FormattedMessage id="productDetail.details.4" />
+            </Typography>
+            <Typography component="li" variant="body1" style={liStyle}>
+              <FormattedMessage id="productDetail.details.5" />
+            </Typography>
+            <Typography component="li" variant="body1" style={liStyle}>
+              <FormattedMessage id="productDetail.details.6" />
+            </Typography>
+          </ul>
+        </Grid>
+        
+        <Grid 
+          item 
+          xs={12} 
+          sm={12} 
+          md={6}
+          className='animate__animated animate__fadeInRight'
+        >
+          {divider}
+          <Typography component="div" variant="h1">
+            <FormattedMessage id="productDetail.characteristics.title" />
+          </Typography>
+          <ul>
+            <Typography component="li" variant="body1" style={liStyle}>
+              <FormattedMessage id="productDetail.characteristics.1" />
+            </Typography>
+            <Typography component="li" variant="body1" style={liStyle}>
+              <FormattedMessage id="productDetail.characteristics.2" />
+            </Typography>
+            <Typography component="li" variant="body1" style={liStyle}>
+              <FormattedMessage id="productDetail.characteristics.3" />
+            </Typography>
+            <Typography component="li" variant="body1" style={liStyle}>
+              <FormattedMessage id="productDetail.characteristics.4" />
+            </Typography>
+          </ul>
+        </Grid>
+        
+        <Grid 
+          item 
+          xs={12} 
+          sm={12} 
+          md={6}
+          className='animate__animated animate__fadeInRight'
+        >
+          {divider}
+          <Typography component="div" variant="h1">
+            <FormattedMessage id="productDetail.shipping.title" />
+          </Typography>
+          <ul>
+            <Typography component="li" variant="body1" style={liStyle}>
+              <FormattedMessage id="productDetail.shipping.1" />
+            </Typography>
+            <Typography component="li" variant="body1" style={liStyle}>
+              <FormattedMessage id="productDetail.shipping.2" />
+            </Typography>
+            <Typography component="li" variant="body1" style={liStyle}>
+              <FormattedMessage id="productDetail.shipping.3" />
+            </Typography>
+          </ul>
+        </Grid>
+
+        <Grid 
+          item 
+          xs={12} 
+          sm={12} 
+          md={6}
+          className='animate__animated animate__fadeInRight'
+        >
+          {divider}
+          <Typography component="div" variant="h1">
+            <FormattedMessage id="productDetail.dimensions.title" />
+          </Typography>
+          <ul>
+            <Typography component="li" variant="body1" style={liStyle}>
+              <FormattedMessage id="productDetail.dimensions.packaging" />
+            </Typography>
+            <Typography component="li" variant="body1" style={liStyle}>
+              <FormattedMessage id="productDetail.dimensions.bags" />
+              <ul>
+                <Typography component="li" variant="body1" style={liStyle}>
+                  <FormattedMessage id="productDetail.dimensions.bags.1" />
+                </Typography>
+                <Typography component="li" variant="body1" style={liStyle}>
+                  <FormattedMessage id="productDetail.dimensions.bags.2" />
+                </Typography>
+              </ul>
+            </Typography>
+          </ul>
+        </Grid>
+        
+        <Grid 
+          item 
+          xs={12} 
+          sm={12} 
+          md={6}
+          className='animate__animated animate__fadeInRight'
+        >
+          {divider}
+          <Typography component="div" variant="body1" sx={{ mb: 2 }}>
+            <FormattedMessage id="productDetail.videoComment.1" />
+          </Typography>
+        </Grid>
+        
+        <Grid 
+          item 
+          xs={12} 
+          sm={12} 
+          md={6}
+          className='animate__animated animate__fadeInRight'
+        >
+          {divider}
+          <Typography component="div" variant="body1">
+            <FormattedMessage id="productDetail.videoComment.2" />
+          </Typography>
+        </Grid>
+
+        <Grid 
+          item 
+          xs={12} 
+          sm={12} 
+          md={6}
+          className='animate__animated animate__fadeInRight'
+        >
+          {divider}
+          <Typography component="div" variant="h1">
+            <FormattedMessage id="productDetail.bags" />
+          </Typography>
+        </Grid>
     </>
   );
 };
