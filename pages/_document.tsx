1c1236c66da873189ea5b40b6f7753acd6b853c5
victorcanas <victorcanas972@gmail.com>
2023-01-13 10:29:23 +0100

    web favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="theme-color" content={theme.palette.primary.main} />
           <meta name="emotion-insertion-point" content="" />
-          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="icon" type="image/png" href="/favicon.png" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
