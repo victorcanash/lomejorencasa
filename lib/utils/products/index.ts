90685fa08c96c843567add67962699848ef1d0e4
victorcanas <victorcanas972@gmail.com>
2023-02-01 20:30:24 +0100

    bags imgs added

--- lib/utils/products/index.ts
+++ lib/utils/products/index.ts
@@ -9,12 +9,15 @@ import everfresh1 from 'public/images/everfresh/everfresh1.jpg';
 import everfresh2 from 'public/images/everfresh/everfresh2.jpg';
 import everfresh3 from 'public/images/everfresh/everfresh3.jpg';
 import everfresh4 from 'public/images/everfresh/everfresh4.jpg';
+import bags1 from 'public/images/bags/bags1.jpg';
+import bags2 from 'public/images/bags/bags2.jpg';
+import bags3 from 'public/images/bags/bags3.jpg';
 
 export const getProductImgUrl = (product: Product, index = 0) => {
   if (everfreshProductId === product.id) {
     return everfresh1;
   } else if (bagProductId === product.id) {
-    return everfresh1;
+    return bags1;
   }
   return getProductImgUrlMW(product, index);
 };
@@ -23,7 +26,7 @@ export const getAllProductImgsUrl = (product: Product) => {
   if (everfreshProductId === product.id) {
     return [everfresh1, everfresh2, everfresh3, everfresh4];
   } else if (bagProductId === product.id) {
-    return [everfresh1, everfresh2, everfresh3, everfresh4];
+    return [bags1, bags2, bags3];
   }
   return getAllProductImgsUrlMW(product);
 };
