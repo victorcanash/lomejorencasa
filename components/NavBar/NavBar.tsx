7c18c5245fdd6c471bd81bf3fea9cd02ce3496e4
victorcanas <victorcanas972@gmail.com>
2023-02-06 13:16:45 +0100

    h1 home in navbar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,16 +1,21 @@
+import { useRouter } from 'next/router';
 import Image from 'next/image';
+
+import { FormattedMessage } from 'react-intl';
 
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
 import Badge from '@mui/material/Badge';
 import MenuIcon from '@mui/icons-material/Menu';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
-import { pages } from '@lib/constants/navigation';
 import Link from '@core/components/Link';
+
+import { pages } from '@lib/constants/navigation';
 import { useCartContext } from '@lib/contexts/CartContext';
 import useDrawer from '@lib/hooks/useDrawer';
 import Drawer from '@components/NavBar/Drawer';
@@ -19,11 +24,12 @@ import logo from 'public/images/navbar-logo.png';
 const NavBar = () => {
   const { totalQuantity } = useCartContext();
 
+  const router = useRouter();
+
   const appDrawer = useDrawer();
 
   return (
     <>
-
       <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, backgroundColor: 'secondary.main' }} onClick={appDrawer.close}>
         <Toolbar variant="dense" disableGutters>
 
@@ -53,6 +59,11 @@ const NavBar = () => {
               href={pages.home.path}
               sx={{ borderRadius: '10px' }}
             >
+              { pages.home.filepath === router.pathname &&
+                <Typography component="h1" variant="h1" sx={{ display: 'none' }}>
+                  <FormattedMessage id="home.h1" />
+                </Typography>
+              }
               <Image
                 src={logo}
                 alt="Logo"
@@ -90,7 +101,6 @@ const NavBar = () => {
         handleOpen={appDrawer.handleOpen}
         handleCollapse={appDrawer.handleCollapse}
       />
-
     </>
   );
 };
