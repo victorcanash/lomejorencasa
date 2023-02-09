1ff185993e2c814d2fe7c95fb536efc0022b2d84
victorcanas <victorcanas972@gmail.com>
2023-02-09 18:51:48 +0100

    product reviews DONE

--- components/products/detail/BagsDetail.tsx
+++ components/products/detail/BagsDetail.tsx
@@ -1,6 +1,15 @@
+import { FormattedMessage } from 'react-intl';
+
 import Grid from '@mui/material/Grid';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
 
 import ProductAccordion from '@components/products/ui/ProductAccordion';
+import ProductReview from '@components/products/ui/ProductReview';
+import review1 from 'public/images/reviews/review1.jpg';
+import review2 from 'public/images/reviews/review2.jpg';
+import review3 from 'public/images/reviews/review3.jpg';
+import review4 from 'public/images/reviews/review4.jpg';
 
 const BagsDetail = () => {
 
@@ -59,6 +68,74 @@ const BagsDetail = () => {
           <ProductAccordion
             textId={'productDetail.refund'}
             itemsCount={2}
+          />
+        </Grid>
+      </Grid>
+
+      <Divider sx={{ mt: 5, mb: 5 }}/>
+
+      <Typography component={"h2"} variant={"h1"} sx={{ mb: 3, textAlign: 'center' }}>
+        <FormattedMessage id="productDetail.reviews.title" />
+      </Typography>
+      <Grid
+        container
+        spacing={2}
+        className='animate__animated animate__fadeIn'
+      >
+        <Grid
+          item 
+          xs={6}
+          sm_md={4}
+        > 
+          <ProductReview
+            imgSrc={review1}
+            username="Jose Antonio"
+            title="Cumple lo especificado."
+            description="La máquina está bastante bien. Tanto el vendedor como el servicio de atención al cliente se podrian puntuar con la máxima puntuación. En principio me llegó una máquina que no funcionaba correctamente, lo comuniqué, el vendedor se puso en contacto conmigo ofreciéndome varias soluciones, le propuse el cambio, y en 2 días tenía en mi poder la nueva máquina. Así da gusto comprar."
+            stars={4}
+            date={new Date('2022-06-02')}
+          />
+        </Grid>
+        <Grid 
+          item 
+          xs={6}
+          sm_md={4}
+        > 
+          <ProductReview
+            imgSrc={review2}
+            username="Alba Sanchez"
+            title="Muy práctico para no gran cantidad de bolsas."
+            description="Me ha gustado su tamaño y su facilidad de ubicarlo, y su eficacia. Hay que tener cuidado con cerrar bien las bolsas."
+            stars={5}
+            date={new Date('2020-12-05')}
+          />
+        </Grid>
+        <Grid 
+          item 
+          xs={6}
+          sm_md={4}
+        > 
+          <ProductReview
+            imgSrc={review3}
+            username="Casinero"
+            title="Recomendable."
+            description="Relación calidad precio inmejorable."
+            stars={5}
+            date={new Date('2021-03-22')}
+          />
+        </Grid>
+        <Grid 
+          item 
+          xs={6}
+          sm_md={4}
+        > 
+          <ProductReview
+            imgSrc={review4}
+            username="Ramón Álvarez"
+            title="Envasa bien."
+            description="Funciona bien, envasa bien. Por ponerle una pega que a veces se escapa el vacío."
+            stars={4}
+            date={new Date('2021-01-02')}
           />
         </Grid>
       </Grid>
---
+++ components/products/ui/ProductReview.tsx
@@ -0,0 +1,96 @@
+import { useRouter } from 'next/router';
+import Image, { StaticImageData } from 'next/image';
+
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import StarOutlinedIcon from '@mui/icons-material/StarOutlined';
+
+type ProductReviewProps = {
+  imgSrc: string | StaticImageData,
+  username: string,
+  title: string,
+  description: string,
+  stars: 0 | 1 | 2 | 3 | 4 | 5,
+  date: Date,
+}
+const ProductReview = (props: ProductReviewProps) => {
+  const {
+    imgSrc,
+    username,
+    title,
+    description,
+    stars,
+    date,
+  } = props;
+
+  const router = useRouter();
+
+  const starsImgs = () => {
+    const fontSize = 20;
+    const starsIcons: JSX.Element[] = [];
+    for (let i = 0; i < 5; i++) {
+      if (stars > i) {
+        starsIcons.push(<StarOutlinedIcon key={i} sx={{ fontSize }}/>);
+      } else {
+        starsIcons.push(<StarBorderOutlinedIcon key={i} sx={{ fontSize }}/>);
+      }
+    }
+    return starsIcons;
+  };
+
+  return (
+    <Box
+      sx={{
+        maxWidth: '300px',
+        margin: 'auto',
+      }}
+    >
+      <Card 
+        sx={{
+          borderRadius: '15px',
+        }}
+      >
+        <CardMedia>
+          <div style={{ position: 'relative', width: '100%' }}>
+            <Image
+              src={imgSrc}
+              alt="Product image"
+              layout="responsive" 
+              objectFit="cover"
+              quality="100"
+            />
+          </div>
+        </CardMedia>
+        <CardContent>
+          <Typography component="div" variant="body2" mb={1}>
+            { username }
+          </Typography>
+          <Grid container mb={1}>
+            <Grid item>
+              {starsImgs()}
+            </Grid>
+          </Grid>
+          <Typography component="div" variant="body2" mb={1}>
+            { title }
+          </Typography>
+          <Typography component="div" variant="body2" mb={1}>
+            {`Revisado en España el ${date.toLocaleDateString(router.locale)}`}
+          </Typography>
+          <Typography component="div" variant="body2" color="#005874" mb={1}>
+            {'Compra verificada'}
+          </Typography>
+          <Typography component="div" variant="body2">
+            { description }
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
+
+export default ProductReview;
