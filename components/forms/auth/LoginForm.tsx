1ec0818f6ad44f7bae51e736af2422fe2cc747ce
victorcanas <victorcanas972@gmail.com>
2023-03-20 17:27:29 +0100

    disable google login now

--- components/forms/auth/LoginForm.tsx
+++ components/forms/auth/LoginForm.tsx
@@ -1,8 +1,8 @@
-import { FormattedMessage } from 'react-intl';
+/*import { FormattedMessage } from 'react-intl';
 
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
-import Box from '@mui/material/Box';
+import Box from '@mui/material/Box';*/
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 import { FormFieldTypes } from '@core/constants/forms';
@@ -10,11 +10,11 @@ import type { AuthLogin } from '@core/types/auth';
 
 import { pages } from '@lib/constants/navigation';
 import type { FormButtonsNormal } from '@lib/types/forms';
-import { useAppContext } from '@lib/contexts/AppContext';
+// import { useAppContext } from '@lib/contexts/AppContext';
 import useForms from '@lib/hooks/useForms';
 import useAuth from '@lib/hooks/useAuth';
 import BaseForm from '@components/forms/BaseForm';
-import GoogleLogin from '@components/google/GoogleLogin';
+// import GoogleLogin from '@components/google/GoogleLogin';
 
 type LoginFormProps = {
   onFailByActivation: (email: string) => void,
@@ -23,10 +23,10 @@ type LoginFormProps = {
 const LoginForm = (props: LoginFormProps) => {
   const { onFailByActivation } = props;
 
-  const { initialized } = useAppContext();
+  // const { initialized } = useAppContext();
 
   const { loginFormValidation, userFieldsInitValues } = useForms();
-  const { login, loginGoogle, errorMsg } = useAuth();
+  const { login, /*loginGoogle, */errorMsg } = useAuth();
 
   const handleSubmit = async (values: AuthLogin) => {
     login(values, onFailByActivation);
@@ -93,7 +93,7 @@ const LoginForm = (props: LoginFormProps) => {
         ]}
       />
 
-      { initialized &&
+      {/* initialized &&
         <Box maxWidth={maxWidth}>
           <Divider
             sx={{
@@ -109,7 +109,7 @@ const LoginForm = (props: LoginFormProps) => {
             login={loginGoogle}
           />
         </Box>
-      }
+      */}
     </>
   );
 };
--- components/forms/auth/RegisterForm.tsx
+++ components/forms/auth/RegisterForm.tsx
@@ -1,8 +1,8 @@
-import { FormattedMessage } from 'react-intl';
+/*import { FormattedMessage } from 'react-intl';
 
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
-import Box from '@mui/material/Box';
+import Box from '@mui/material/Box';*/
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 import { FormFieldTypes } from '@core/constants/forms';
@@ -10,11 +10,11 @@ import type { AuthRegister } from '@core/types/auth';
 
 import { pages } from '@lib/constants/navigation';
 import type { FormButtonsNormal } from '@lib/types/forms';
-import { useAppContext } from '@lib/contexts/AppContext';
+// import { useAppContext } from '@lib/contexts/AppContext';
 import useForms from '@lib/hooks/useForms';
 import useAuth from '@lib/hooks/useAuth';
 import BaseForm from '@components/forms/BaseForm';
-import GoogleLogin from '@components/google/GoogleLogin';
+// import GoogleLogin from '@components/google/GoogleLogin';
 
 type RegisterFormProps = {
   onSuccess: (email: string) => void,
@@ -23,10 +23,10 @@ type RegisterFormProps = {
 const RegisterForm = (props: RegisterFormProps) => {
   const { onSuccess } = props;
 
-  const { initialized } = useAppContext();
+  // const { initialized } = useAppContext();
 
   const { registerFormValidation, userFieldsInitValues } = useForms();
-  const { register, loginGoogle, errorMsg } = useAuth();
+  const { register, /*loginGoogle, */errorMsg } = useAuth();
 
   const handleSubmit = async (values: AuthRegister) => {
     register(values, onSuccess);
@@ -111,7 +111,7 @@ const RegisterForm = (props: RegisterFormProps) => {
         ]}
       />
 
-      { initialized &&
+      {/* initialized &&
         <Box maxWidth={maxWidth}>
           <Divider
             sx={{
@@ -127,7 +127,7 @@ const RegisterForm = (props: RegisterFormProps) => {
             login={loginGoogle}
           />
         </Box>
-      }
+      */}
     </>
   );
 };
