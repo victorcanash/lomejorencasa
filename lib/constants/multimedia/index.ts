f0e5d5d5d40f617758040a184ce0f197c37bbf0c
Victor Canas <victorcanas972@gmail.com>
2023-04-07 13:11:47 +0200

    fix to deploy

--- lib/constants/multimedia/index.ts
+++ lib/constants/multimedia/index.ts
@@ -1,4 +1,4 @@
-const everfreshProductImgs = [
+export const everfreshProductImgs = [
   'v1680715068/laenvasadora/LANDING%20PAGE/FOTO_4_duno3s.jpg',
   'v1680726536/laenvasadora/LANDING%20PAGE/FOTO_2_fgnlnf.jpg',
   'v1680726664/laenvasadora/LANDING%20PAGE/FOTO_5_g6vcri.jpg',
