91340bf51791d5145de99ddb51ebccd814999037
Victor Canas <victorcanas972@gmail.com>
2023-04-14 12:25:32 +0200

    fix everfresh and bags images

--- lib/constants/multimedia/index.ts
+++ lib/constants/multimedia/index.ts
@@ -4,15 +4,14 @@ export const placeholderImgId = 'v1681059169/laenvasadora/LANDING%20PAGE/placeho
 
 export const everfreshImgIds = [
   'v1680715068/laenvasadora/LANDING%20PAGE/FOTO_4_duno3s.jpg',
-  'v1680726536/laenvasadora/LANDING%20PAGE/FOTO_2_fgnlnf.jpg',
+  'v1680878929/laenvasadora/LANDING%20PAGE/FOTO_3_lzky3y.jpg',
   'v1680726664/laenvasadora/LANDING%20PAGE/FOTO_5_g6vcri.jpg',
   'v1680726760/laenvasadora/LANDING%20PAGE/FOTO_1_yarwhg.jpg',
-  'v1680878929/laenvasadora/LANDING%20PAGE/FOTO_3_lzky3y.jpg',
 ];
 
 export const bagsImgIds = [
-  'v1680878418/laenvasadora/LANDING%20PAGE/bolsas_ayqf6k.jpg',
   'v1680692733/laenvasadora/LANDING%20PAGE/Foto_bolsas_detalle_cuadrada_jcsiul.jpg',
+  'v1680878418/laenvasadora/LANDING%20PAGE/bolsas_ayqf6k.jpg',
   'v1680692777/laenvasadora/LANDING%20PAGE/Grafico-bolsas-letras-grandes_d8pl7r.jpg',
 ];
 
