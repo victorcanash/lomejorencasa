fc75e3056040ac0eb1dc9e7da722ddb83c553df9
Victor Canas <victorcanas972@gmail.com>
2023-04-14 11:54:45 +0200

    fix scroll with addedY on nav

--- core/utils/navigation/index.ts
+++ core/utils/navigation/index.ts
@@ -1,4 +1,4 @@
-export const scrollToSection = (id?: string) => {
+export const scrollToSection = (id?: string, addedY = 121) => {
   setTimeout(() => {
     if (!id) {
       id = window.location.hash.replace('#', '');
@@ -7,7 +7,7 @@ export const scrollToSection = (id?: string) => {
     if (element) {
       const r = element.getBoundingClientRect();
       window.top?.scroll({
-        top: scrollY + r.top,
+        top: scrollY + (r.top - addedY),
         behavior: 'smooth',
       });
     }
