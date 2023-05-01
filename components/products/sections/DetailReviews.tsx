d15bbc9e752b8274b0a8d95cfcaacb418babe222
Victor Canas <victorcanas972@gmail.com>
2023-05-01 19:47:49 +0200

    detail reviews fixed

--- components/products/sections/DetailReviews.tsx
+++ components/products/sections/DetailReviews.tsx
@@ -38,145 +38,156 @@ const DetailReviews = () => {
   const { errorMsg, successMsg, handleChangePage, createProductReview} = useReviews();
 
   return (
-    <Container id="reviews">
+    <>
       { initialized &&
-        <Box 
-          maxWidth="md"
-          m="auto"
-        >
-          <Title
-            type="h2"
-            texts={{
-              title: {
-                id: 'productDetail.reviews.title'
-              },
-            }}
-            divider={true}
-          />
-          {/* Create ProductReview Form */}
-          <Box mb={4}>
-            <ProductReviewForm
-              errorMsg={errorMsg}
-              successMsg={successMsg}
-              createProductReview={createProductReview}
-            />
-          </Box>
-          {/* ProductReview List */}
-          { listProductReviews.reviews.length > 0 ?
-            <Masonry columns={{ xs: 2, sm: 2, md: 3 }} spacing={0}>
-              { listProductReviews.reviews.map((item, index) => (
-                <Box key={index}>
-                  <Box
-                    sx={{
-                      maxWidth: '350px',
-                      m: 'auto',
-                      mb: {
-                        xs: 1,
-                        sm: 2,
-                      },
-                      px: {
-                        xs: 0.5,
-                        sm: 1,
-                      }
-                    }}
-                  >
-                    <Card 
-                      sx={{
-                        borderRadius: '15px',
-                      }}
-                    >
-                      <CardHeader
-                        title={
-                          <Grid container justifyContent="space-between">
-                            <Grid item>
-                              <Rating
-                                value={item.rating}
-                                precision={0.5}
-                                readOnly
-                              />        
-                            </Grid>
-                            <Grid item>
-                              <Typography component="div" variant="body2" color="text.disabled" width="min-content">
-                                { convertToDate(item.createdAt, router.locale) }
-                              </Typography>
-                            </Grid>
-                          </Grid>
-                        }
-                        subheader={
-                          <>
-                            <Typography component="div" variant="body1" color="text.primary">
-                              {item.publicName}
-                            </Typography>
-                            <Typography component="div" variant="body2" sx={{ color: colors.text.verified }}>
-                              <FormattedMessage
-                                id="productDetail.reviews.verified"
-                              />
-                              <Box component="span" sx={{ ml: '4px' }}>
-                                <FontAwesomeIcon icon={faCircleCheck} />
-                              </Box>
-                            </Typography>
-                          </>
-                        }
-                      />
-                      { item.imageUrl &&
-                        <CardMedia>
-                          <Box
-                            sx={{
-                              position: 'relative',
-                              width: '100%',
-                              margin: 'auto',
-                              paddingTop: '100%',
-                            }}
-                          >
-                            <CustomImage
-                              src={item.imageUrl}
-                              alt={keywords.vacuumMachine.main}
-                              layout="fill"
-                              objectFit="cover"
-                            />
-                          </Box>
-                        </CardMedia>
-                      }
-                      <CardContent
+        <>
+          <Container id="reviews">
+            <Box 
+              maxWidth="md"
+              m="auto"
+            >
+              <Title
+                type="h2"
+                texts={{
+                  title: {
+                    id: 'productDetail.reviews.title'
+                  },
+                }}
+                divider={true}
+              />
+              {/* Create ProductReview Form */}
+              <Box mb={4}>
+                <ProductReviewForm
+                  errorMsg={errorMsg}
+                  successMsg={successMsg}
+                  createProductReview={createProductReview}
+                />
+              </Box>
+            </Box>
+          </Container>
+          <Container sx={{ px: { xs: 1.5, sm: 2 } }}>
+            <Box 
+              maxWidth="md"
+              m="auto"
+            >
+              {/* ProductReview List */}
+              { listProductReviews.reviews.length > 0 ?
+                <Masonry columns={{ xs: 2, sm: 2, md: 3 }} spacing={0}>
+                  { listProductReviews.reviews.map((item, index) => (
+                    <Box key={index}>
+                      <Box
                         sx={{
-                          mt: item.imageUrl ? undefined : -2,
+                          maxWidth: '350px',
+                          m: 'auto',
+                          mb: {
+                            xs: 1,
+                            sm: 2,
+                          },
+                          px: {
+                            xs: 0.5,
+                            sm: 1,
+                          }
                         }}
                       >
-                        <Typography component="div" variant="body1Head" mb={1} sx={{ fontSize: '16px', fontWeight: '600' }}>
-                          {
-                            item.pack ?
-                              getPackGeneralName(item.pack, allLandingConfigs) : item.product?.name.current
+                        <Card 
+                          sx={{
+                            borderRadius: '15px',
+                          }}
+                        >
+                          <CardHeader
+                            title={
+                              <Grid container justifyContent="space-between">
+                                <Grid item>
+                                  <Rating
+                                    value={item.rating}
+                                    precision={0.5}
+                                    readOnly
+                                  />        
+                                </Grid>
+                                <Grid item>
+                                  <Typography component="div" variant="body2" color="text.disabled" width="min-content">
+                                    { convertToDate(item.createdAt, router.locale) }
+                                  </Typography>
+                                </Grid>
+                              </Grid>
+                            }
+                            subheader={
+                              <>
+                                <Typography component="div" variant="body1" color="text.primary">
+                                  {item.publicName}
+                                </Typography>
+                                <Typography component="div" variant="body2" sx={{ color: colors.text.verified }}>
+                                  <FormattedMessage
+                                    id="productDetail.reviews.verified"
+                                  />
+                                  <Box component="span" sx={{ ml: '4px' }}>
+                                    <FontAwesomeIcon icon={faCircleCheck} />
+                                  </Box>
+                                </Typography>
+                              </>
+                            }
+                          />
+                          { item.imageUrl &&
+                            <CardMedia>
+                              <Box
+                                sx={{
+                                  position: 'relative',
+                                  width: '100%',
+                                  margin: 'auto',
+                                  paddingTop: '100%',
+                                }}
+                              >
+                                <CustomImage
+                                  src={item.imageUrl}
+                                  alt={keywords.vacuumMachine.main}
+                                  layout="fill"
+                                  objectFit="cover"
+                                />
+                              </Box>
+                            </CardMedia>
                           }
-                        </Typography>
-                        <Typography component="div" variant="body1Head" mb={1}>
-                          { item.title }
-                        </Typography>
-                        <Typography component="div" variant="body1">
-                          { item.description }
-                        </Typography>
-                      </CardContent>
-                    </Card>
-                  </Box>
-                </Box>
-              ))}
-            </Masonry>
-            :
-            <Typography component="h3" variant="body1" sx={{ textAlign: "center" }}>
-              <FormattedMessage
-                id="productDetail.reviews.noItems"
+                          <CardContent
+                            sx={{
+                              mt: item.imageUrl ? undefined : -2,
+                            }}
+                          >
+                            <Typography component="div" variant="body1Head" mb={1} sx={{ fontSize: '16px', fontWeight: '600' }}>
+                              {
+                                item.pack ?
+                                  getPackGeneralName(item.pack, allLandingConfigs) : item.product?.name.current
+                              }
+                            </Typography>
+                            <Typography component="div" variant="body1Head" mb={1}>
+                              { item.title }
+                            </Typography>
+                            <Typography component="div" variant="body1">
+                              { item.description }
+                            </Typography>
+                          </CardContent>
+                        </Card>
+                      </Box>
+                    </Box>
+                  ))}
+                </Masonry>
+                :
+                <Typography component="h3" variant="body1" sx={{ textAlign: "center" }}>
+                  <FormattedMessage
+                    id="productDetail.reviews.noItems"
+                  />
+                </Typography>
+              }
+              {/* Pagination */}
+              <Box mt={listProductReviews.reviews.length > 0 ? 3 : 5} />
+              <Pagination
+                totalPages={listProductReviews.totalPages}
+                currentPage={listProductReviews.currentPage}
+                onChangePage={handleChangePage}
               />
-            </Typography>
-          }
-          {/* Pagination */}
-          <Box mt={listProductReviews.reviews.length > 0 ? 3 : 5} />
-          <Pagination
-            totalPages={listProductReviews.totalPages}
-            currentPage={listProductReviews.currentPage}
-            onChangePage={handleChangePage}
-          />
-        </Box>
+            </Box>
+          </Container>
+        </>
       }
-    </Container>
+    </>
   );
 };
 
