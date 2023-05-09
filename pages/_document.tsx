3971c1419b891c36c89a0293498074af72de1d78
Victor Canas <victorcanas972@gmail.com>
2023-05-09 15:50:12 +0200

    img pixel

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import Image from 'next/image';
@@ -32,10 +34,9 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
           />
           <noscript>
-            <Image
+            <img
               height="1"
               width="1"
-              alt=""
               style={{ display: 'none' }}
               src={`https://www.facebook.com/tr?id=${envConfig.NEXT_PUBLIC_FB_PIXEL_ID}&ev=PageView&noscript=1`}
             />
