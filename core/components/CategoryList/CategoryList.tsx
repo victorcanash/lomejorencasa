b7cd56e3de9dae32c3255f1b9d6929d4e35af3bc
Victor Canas <victorcanas972@gmail.com>
2023-06-13 11:56:33 +0200

    fix category list style

--- core/components/CategoryList/CategoryList.tsx
+++ core/components/CategoryList/CategoryList.tsx
@@ -22,64 +22,70 @@ const CategoryList = () => {
         overflowX: 'auto',
       }}
     >
-      <Stack
-        direction="row"
-        spacing={2}
+      <Box 
+        maxWidth="md"
+        m="auto"
       >
-        { getAllCategories().map((category) => (
-          <Box
-            key={category.id}
-            sx={{
-              position: 'relative',
-              width: 'max-content',
-              minWidth: {
-                xs: '26%',
-                md: '20%',
-                md_lg: '200px',
-              },
-            }}
-          >
+        <Stack
+          direction="row"
+          spacing={2}
+          pb={1}
+        >
+          { getAllCategories().map((category) => (
             <Box
+              key={category.id}
               sx={{
                 position: 'relative',
-                width: '100%',
-                paddingTop: '100%',
+                width: 'max-content',
+                minWidth: {
+                  xs: '26%',
+                  sm_md: '22%',
+                },
               }}
+
             >
-              <Avatar
+              <Box
                 sx={{
-                  position: 'absolute',
+                  position: 'relative',
                   width: '100%',
-                  height: '100%',
-                  top: '50%',
-                  left: '50%',
-                  transform: 'translate(-50%, -50%)',
+                  paddingTop: '100%',
                 }}
               >
-                <CustomImage
-                  alt={category.name.current}
-                  src={getItemImgUrl(category)}          
-                  layout="fill" 
-                />
-              </Avatar>
+                <Avatar
+                  sx={{
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                  }}
+                >
+                  <CustomImage
+                    alt={category.name.current}
+                    src={getItemImgUrl(category)}          
+                    layout="fill" 
+                  />
+                </Avatar>
+              </Box>
+              <Typography
+                component="div"
+                variant="body1Head"
+                m="auto"
+                mt={1}
+                sx={{
+                  ...themeCustomElements.categoryList?.nameText?
+                    convertElementToSx(themeCustomElements.categoryList.nameText) : undefined,
+                  wordWrap: 'break-word',
+                }}
+              >
+                { category.name.current }
+              </Typography>
             </Box>
-            <Typography
-              component="div"
-              variant="body1Head"
-              m="auto"
-              mt={1}
-              sx={{
-                ...themeCustomElements.categoryList?.nameText?
-                  convertElementToSx(themeCustomElements.categoryList.nameText) : undefined,
-                wordWrap: 'break-word',
-              }}
-            >
-              { category.name.current }
-            </Typography>
-          </Box>
-        ))}
-        <Box sx={{ width: '250px' }} />
-      </Stack>
+          ))}
+          <Box pl={1} sx={{ width: { xs: '250px', sm_md: '350px' } }} />
+        </Stack>
+      </Box>
     </Container>
   );
 };
