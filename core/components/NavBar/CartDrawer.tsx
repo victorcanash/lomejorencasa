81d15dce52695180160b67c1d07dea76ddcf750f
Victor Canas <victorcanas972@gmail.com>
2023-06-14 11:19:17 +0200

    title and close button in cart drawer

--- core/components/NavBar/CartDrawer.tsx
+++ core/components/NavBar/CartDrawer.tsx
@@ -1,8 +1,15 @@
+import { FormattedMessage } from 'react-intl';
+
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 
 import { useCartContext } from '@core/contexts/CartContext';
+import Divider from '@core/components/ui/Divider';
 import CartDetail from '@core/components/CartDetail';
 
 type CartDrawerProps = {
@@ -50,6 +57,25 @@ const CartDrawer = (props: CartDrawerProps) => {
           p: '16px',
         }}
       >
+        {/* Title */}
+        <Grid container justifyContent="space-between">
+          <Grid item>
+            <Typography component="div" variant="h2" p="10px">
+              <FormattedMessage id="cart.h1" />
+            </Typography>
+          </Grid>
+          <Grid item>
+            <IconButton
+              onClick={handleDrawerOpen}
+            >
+              <CloseIcon fontSize="large" />
+            </IconButton>
+          </Grid>
+        </Grid>
+        <Divider
+          mb={3}
+        />
+        {/* Cart Detail */}
         <CartDetail />
       </Box>
     </Drawer>
